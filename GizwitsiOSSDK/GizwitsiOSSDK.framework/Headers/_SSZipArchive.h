4ebdf350c8242e4678b5e66714e9b84a69cf1f2f
王锐彬 <492960423@qq.com>
2024-01-25 19:16:09 +0800

    no message

--- GizwitsiOSSDK/GizwitsiOSSDK.framework/Headers/_SSZipArchive.h
+++ GizwitsiOSSDK/GizwitsiOSSDK.framework/Headers/_SSZipArchive.h
@@ -10,7 +10,7 @@
 
 #import <Foundation/Foundation.h>
 
-#import "SSZipCommon.h"
+#import "_SSZipCommon.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
--- GizwitsiOSSDK/GizwitsiOSSDK.framework/Headers/_ZipArchive.h
+++ GizwitsiOSSDK/GizwitsiOSSDK.framework/Headers/_ZipArchive.h
@@ -16,10 +16,11 @@ FOUNDATION_EXPORT const unsigned char ZipArchiveVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <SSZipArchive.h>
 
 // This is to account for the many different ways this library gets imported.
-#if __has_include(<SSZipArchive/SSZipArchive.h>)
-#import <SSZipArchive/SSZipArchive.h>
-#elif __has_include("../SSZipArchive.h")
-#import "../SSZipArchive.h"
-#else
-#import "SSZipArchive.h"
-#endif
+//#if __has_include(<SSZipArchive/SSZipArchive.h>)
+//#import <SSZipArchive/SSZipArchive.h>
+//#elif __has_include("../_SSZipArchive.h")
+//#import "../_SSZipArchive.h"
+//#else
+//#import "_SSZipArchive.h"
+//#endif
+#import "_SSZipArchive.h"
